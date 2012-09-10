92008e90e9c2dafa5f20940c07120b0e71740719
SergioJorge <sergiojcamposjr@gmail.com>
2012-09-10 14:56:47 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globotalk::Application.config.secret_token = 'f3d030a2c3b54435f7e1ccd552c3447066b09ed0bf4d5a2451d6c23ff740349eb370505f85629530ca1589d27cc0a6078885c5aecbd4b3b10a3420dc4b027872'
