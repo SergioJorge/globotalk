117d2542aeecfe8884711a065bdf9e70d3f881c0
sergiojunior <sergio.junior@corp.globo.com>
2012-09-19 01:27:14 -0300

    Agora campo data é um date não mais um datetime

--- db/migrate/20120911200314_create_talks.rb
+++ db/migrate/20120911200314_create_talks.rb
@@ -2,7 +2,7 @@ class CreateTalks < ActiveRecord::Migration
   def change
     create_table :talks do |t|
       t.string :nome
-      t.datetime :data
+      t.date :data
       t.string :responsavel
       t.text :assunto
       t.text :mais_sobre
